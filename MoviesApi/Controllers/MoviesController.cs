4c3010078766b24f602fff466266b6ad0a585a8c
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-07-26 17:19:30 +0300

    Add [POST] Create Movie Endpoint - Part 2

--- MoviesApi/Controllers/MoviesController.cs
+++ MoviesApi/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MoviesApi.Controllers
 {
@@ -8,6 +9,8 @@ namespace MoviesApi.Controllers
     public class MoviesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private new List<string> _allowedExtenstions = new List<string> { ".jpg", ".png" };
+        private long _maxAllowedPosterSize = 1048576;
 
         public MoviesController(ApplicationDbContext context)
         {
@@ -16,8 +19,18 @@ namespace MoviesApi.Controllers
 
         [HttpPost]   
 
-        public async    Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
+        public async  Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
         {
+            if (!_allowedExtenstions.Contains(Path.GetExtension(dto.poster.FileName).ToLower()))
+                return BadRequest("Only .png and .jpg images are allowed!");
+
+            if (dto.poster.Length > _maxAllowedPosterSize)
+                return BadRequest("Max allowed size for poster is 1MB!");
+
+            var isValidGenre = await _context.Genres.AnyAsync(g => g.id == dto.GenreId);
+            if (!isValidGenre)
+                return BadRequest("Invalid genere ID!");
+
             using var dataStream= new MemoryStream();
 
             await dto.poster.CopyToAsync(dataStream);
